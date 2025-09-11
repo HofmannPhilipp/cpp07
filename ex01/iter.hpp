0ee3ddfb63f8dea84a959b4c15896d8bf567686f
Philipp Hofmann <phhofman@3-F-13.42heilbronn.de>
2025-09-11 10:42:20 +0200

    changed in ex01 the parameter Array of iter to be a referenece

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,14 +6,14 @@
 /*   By: phhofman <phhofman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 14:45:25 by phhofman          #+#    #+#             */
-/*   Updated: 2025/09/10 09:57:02 by phhofman         ###   ########.fr       */
+/*   Updated: 2025/09/11 10:39:14 by phhofman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 template <typename Array, typename Length, typename Func>
-void iter(Array arr, Length len, Func f)
+void iter(Array &arr, Length len, Func f)
 {
     for (Length i = 0; i < len; i++)
         f(arr[i]);
